ba347320549a881dca528920650d7d2f6f103a3d
iamfreedoky <149290020+iamfreedoky@users.noreply.github.com>
2023-10-29 00:06:17 -0700

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,6 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
-export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
+export const APP_ID = `c1a31054-bcca-45eb-aca5-d007aba234f3`
+export const API_KEY = `app-MFOAB5MKw3LUjTpmk7Ngn0GC`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
   title: 'Chat APP',
